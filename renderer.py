15ad4e9e3fcb7b6d9a5731f1f5f4a5e54985353a
Keith <hnefatl@gmail.com>
2022-12-31 04:21:52 +0100

    Slight restructure

--- renderer.py
+++ renderer.py
@@ -34,6 +34,42 @@ def _draw_slice(
     pygame.draw.polygon(surface, (0, 0, 0), vertices, width=2)
 
 
+def _render_wheel(surface: pygame.surface.Surface, zones: set[str]):
+    arc_angle = 2 * pi / len(zones)
+    even_colour = pygame.color.Color(150, 150, 0)
+    odd_colour = pygame.color.Color(128, 128, 0)
+    for i, _ in enumerate(zones):
+        colour = even_colour if i % 2 == 0 else odd_colour
+        left_angle = pi / 2 - i * arc_angle
+        _draw_slice(
+            surface,
+            colour,
+            surface.get_rect(),
+            left_angle - arc_angle,
+            left_angle,
+        )
+
+def _render_hands(
+    surface: pygame.surface.Surface, 
+    font: pygame.font.Font,
+    people: set[PersonState],
+    failures: dict[Person, HomeassistantAPIError],
+):
+    current_height = 0
+
+    for person in people:
+        text = font.render(
+            f"{person.person.name}: {person.get_location()}", True, (0, 255, 0)
+        )
+        surface.blit(text, (0, current_height))
+        current_height += font.get_height()
+
+    for person, failure in failures.items():
+        text = font.render(f"{person.name}: {failure}", True, (255, 0, 0))
+        surface.blit(text, (0, current_height))
+        current_height += font.get_height()
+
+
 class Renderer:
     def __init__(self):
         pass
@@ -42,8 +78,9 @@ class Renderer:
         pygame.init()
         self._screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # Make a square surface just for rendering the clock itself, for simpler dimensions
-        self._clock_area = pygame.rect.Rect(0, 0, 1, 1).fit(self._screen.get_rect())
-        self._clock_area.center = self._screen.get_rect().center
+        clock_area = pygame.rect.Rect(0, 0, 1, 1).fit(self._screen.get_rect())
+        clock_area.center = self._screen.get_rect().center
+        self._clock_surface = self._screen.subsurface(clock_area)
         self._width = self._screen.get_width()
         self._height = self._screen.get_height()
 
@@ -62,43 +99,9 @@ class Renderer:
         zones: set[str],
     ):
         self._screen.fill((0, 0, 0))
-        self._render_wheel(zones)
-        self._render_hands(people, failures)
+        _render_wheel(self._clock_surface, zones)
+        _render_hands(self._screen, self._font, people, failures)
         pygame.display.flip()
-
-    def _render_wheel(self, zones: set[str]):
-        arc_angle = 2 * pi / len(zones)
-        even_colour = pygame.color.Color(150, 150, 0)
-        odd_colour = pygame.color.Color(128, 128, 0)
-        for i, _ in enumerate(zones):
-            colour = even_colour if i % 2 == 0 else odd_colour
-            left_angle = pi / 2 - i * arc_angle
-            _draw_slice(
-                self._screen,
-                colour,
-                self._clock_area,
-                left_angle - arc_angle,
-                left_angle,
-            )
-
-    def _render_hands(
-        self,
-        people: set[PersonState],
-        failures: dict[Person, HomeassistantAPIError],
-    ):
-        current_height = 0
-
-        for person in people:
-            text = self._font.render(
-                f"{person.person.name}: {person.get_location()}", True, (0, 255, 0)
-            )
-            self._screen.blit(text, (0, current_height))
-            current_height += self._font.get_height()
-
-        for person, failure in failures.items():
-            text = self._font.render(f"{person.name}: {failure}", True, (255, 0, 0))
-            self._screen.blit(text, (0, current_height))
-            current_height += self._font.get_height()
 
     def should_exit(self) -> bool:
         events = pygame.event.get(eventtype=pygame.KEYDOWN)
